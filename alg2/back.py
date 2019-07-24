a3e05a3c1b50b392710bdae1bf8e003a101ed316
mk668a <msftoaa@gmail.com>
2019-07-24 22:10:02 +0900

    create 8-queens

--- alg2/back.py
+++ alg2/back.py
@@ -20,28 +20,23 @@ def DivideMatrix(m, n, n_2):
 
 
 def deleteMatrix(a, b):
-    c = np.array(a) - np.array(b)
-    return c.tolist()
-    # print(a)
-    # n = len(a)
-    # c = []
-    # for i in range(n):
-    #     c.append([])
-    #     for j in range(len(a[i])):
-    #         c[i].append(a[i][j] - b[i][j])
-    # return c
+    n = len(a)
+    c = []
+    for i in range(n):
+        c.append([])
+        for j in range(len(a[i])):
+            c[i].append(a[i][j] - b[i][j])
+    return c
 
 
 def sumMatrix(a, b):
-    c = np.array(a) + np.array(b)
-    return c.tolist()
-    # n = len(a)
-    # c = []
-    # for i in range(n):
-    #     c.append([])
-    #     for j in range(len(a[i])):
-    #         c[i].append(a[i][j] + b[i][j])
-    # return c
+    n = len(a)
+    c = []
+    for i in range(n):
+        c.append([])
+        for j in range(len(a[i])):
+            c[i].append(a[i][j] + b[i][j])
+    return c
 
 
 def MAT_MULT(A_, B_):
@@ -65,10 +60,10 @@ def MAT_MULT(A_, B_):
         b21 = B[1]
         b22 = B[3]
 
-        C[0] = sumMatrix(MAT_MULT(a11, b11), MAT_MULT(a12, b21))
-        C[1] = sumMatrix(MAT_MULT(a11, b12), MAT_MULT(a12, b22))
-        C[2] = sumMatrix(MAT_MULT(a21, b11), MAT_MULT(a22, b21))
-        C[3] = sumMatrix(MAT_MULT(a21, b12), MAT_MULT(a22, b22))
+        C[0] = sumMatrix(MAT_MULT(a11, b11), MAT_MULT(a12, b21))[0]
+        C[1] = sumMatrix(MAT_MULT(a11, b12), MAT_MULT(a12, b22))[0]
+        C[2] = sumMatrix(MAT_MULT(a21, b11), MAT_MULT(a22, b21))[0]
+        C[3] = sumMatrix(MAT_MULT(a21, b12), MAT_MULT(a22, b22))[0]
 
         return C
 
@@ -113,10 +108,10 @@ def Strassens(A_, B_):
         P6 = Strassens(S7, S8)
         P7 = Strassens(S9, S10)
 
-        C[0] = deleteMatrix(sumMatrix(P5, P4), sumMatrix(P2, P6))
-        C[1] = sumMatrix(P1, P2)
-        C[2] = sumMatrix(P3, P4)
-        C[3] = deleteMatrix(sumMatrix(P1, P5), deleteMatrix(P3, P7))
+        C[0] = deleteMatrix(sumMatrix(P5, P4), sumMatrix(P2, P6))[0]
+        C[1] = sumMatrix(P1, P2)[0]
+        C[2] = sumMatrix(P3, P4)[0]
+        C[3] = deleteMatrix(sumMatrix(P1, P5), deleteMatrix(P3, P7))[0]
 
         return C
 
@@ -133,49 +128,49 @@ def CreateMatrix(m, n):
 
 
 # テスト
-# 分割
-# A_ = [[2, 3, 4, 6], [1, 4, 2, 7], [2, 1, 3, 6], [3, 4, 5, 6]]
-# print(A_)
-# A = DivideMatrix(A_, len(A_), len(A_)/2)
-# print(A)
-
-# A = [[1, 3], [7, 5]]
-# B = [[6, 8], [4, 2]]
 # A = [[7, 5]]
 # B = [[4, 2]]
-A = [[2, 3, 4, 6], [1, 4, 2, 7], [2, 1, 3, 6], [3, 4, 5, 6]]
-B = [[3, 1, 2, 1], [2, 4, 2, 4], [4, 3, 5, 3], [6, 3, 5, 5]]
-len_A = len(A)
-len_B = len(B)
+A = [[1, 3], [7, 5]]
+B = [[6, 8], [4, 2]]
+# A = [[2, 3, 4, 6], [1, 4, 2, 7], [2, 1, 3, 6], [3, 4, 5, 6]]
+# B = [[3, 1, 2, 1], [2, 4, 2, 4], [4, 3, 5, 3], [6, 3, 5, 5]]
+# n = 64
+# A = CreateMatrix(n, n)
+# B = CreateMatrix(n, n)
+
+# 分割
+# print(A)
+# A = DivideMatrix(A, len(A), len(A)/2)
+# print(A)
+
+# 和
+# C = sumMatrix(A, B)
+
 C = MAT_MULT(A, B)
 print(C)
+
 C = Strassens(A, B)
 print(C)
 
-matrix = [32, 64, 128, 256, 512, 1024]
-for n in matrix:
-    print("matrix: ", n, " * ", n)
-    A = CreateMatrix(n, n)
-    B = CreateMatrix(n, n)
-    A = np.matrix(A)
-    B = np.matrix(B)
-    len_A = len(A)
-    len_B = len(B)
-    C = [[0 for i in range(len(A))]for j in range(len(A))]
-
-    Recursive_s = time.time()
-    C = MAT_MULT(A, B)
-    Recursive_e = time.time()
-    # 計算結果
-    # for c in C:
-    #     print(c)
-    print("Recursive:", round(Recursive_e - Recursive_s, 8))
-
-    Strassen_s = time.time()
-    C = Strassens(A, B)
-    Strassen_e = time.time()
-    # 計算結果
-    # for c in C:
-    #     print(c)
-    print("Strassens:", round(Strassen_e - Strassen_s, 8))
-    print()
+# matrix = [32, 64, 128, 256, 512, 1024]
+# for n in matrix:
+#     print("matrix: ", n, " * ", n)
+#     A = CreateMatrix(n, n)
+#     B = CreateMatrix(n, n)
+#
+#     Recursive_s = time.time()
+#     C = MAT_MULT(A, B)
+#     Recursive_e = time.time()
+#     # 計算結果
+#     # for c in C:
+#     #     print(c)
+#     print("Recursive:", round(Recursive_e - Recursive_s, 8))
+#
+#     Strassen_s = time.time()
+#     C = Strassens(A, B)
+#     Strassen_e = time.time()
+#     # 計算結果
+#     # for c in C:
+#     #     print(c)
+#     print("Strassens:", round(Strassen_e - Strassen_s, 8))
+#     print()
