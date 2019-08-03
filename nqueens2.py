cf05594327bba10dcaab056566e0f4512cab5ac8
mk668a <msftoaa@gmail.com>
2019-08-03 16:15:39 +0900

    nqueens2

--- nqueens2.py
+++ nqueens2.py
@@ -9,6 +9,7 @@ def n_queen(n):
 
 
 def put_queens(board, y=0):
+    # print("y =", y)
     n = len(board)
     if y == n:
         yield board
@@ -16,11 +17,15 @@ def put_queens(board, y=0):
         for x in range(n):
             if not conflict(board, x, y):
                 board[y] = x
+                # print("true", "x =", x, board)
                 yield from put_queens(board, y + 1)
-                board[y] = n
+            # print("false", "x =", x, board)
 
 
 def conflict(board, x, y):
+    # for ty, tx in enumerate(board[:y]):
+    #     if tx == x or tx - ty + y == x or tx + ty - y == x:
+    #         return True
     return any(tx == x or tx - ty + y == x or tx + ty - y == x
                for ty, tx in enumerate(board[:y]))
 
